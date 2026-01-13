0f0541b6795587fd487661836e08069929b52e3e
jose <164822269+0Darkn@users.noreply.github.com>
2026-01-13 15:52:53 +0000

    Create script.js

---
+++ web/script.js
@@ -0,0 +1,10 @@
+/*
+JavaScript simples apenas para mostrar
+que a página é dinâmica
+*/
+
+console.log("Página carregada com sucesso!");
+
+setInterval(() => {
+    console.log("Servidor Flask ativo...");
+}, 3000);
